3c42980eea10fd3ccc0461fc87c0997637fa682d
RamonPetitinga <ramonlpbahia@gmail.com>
2024-12-26 22:01:29 -0300

    Alteração no Menu de opções

--- biblioteca/massa.h
+++ biblioteca/massa.h
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 #include <locale.h>
 
 // Funções para conversão de unidades
@@ -9,67 +8,88 @@ double gramas_para_quilogramas(double gramas)
     return gramas / 1000.0;
 }
 
-double gramas_para_libras(double gramas)
+double quilogramas_para_gramas(double quilogramas)
 {
-    return gramas / 453.592;
+    return quilogramas * 1000;
 }
 
-double quilogramas_para_libras(double quilogramas)
+double toneladas_para_gramas(double toneladas)
 {
-    return quilogramas * 2.20462;
+    return toneladas * 1000000;
 }
 
-double libras_para_quilogramas(double libras)
+double toneladas_para_quilogramas(double toneladas)
 {
-    return libras * 0.453592;
+    return toneladas * 1000;
 }
 
-void conversordemassa()
+double gramas_para_toneladas(double gramas)
 {
+    return gramas / 1000000;
+}
 
+double quilogramas_para_toneladas(double quilogramas)
+{
+    return quilogramas / 1000;
+}
+
+void conversao_massa()
+{
     setlocale(LC_ALL, "Portuguese_Brazil");
     int opcao;
     double valor;
 
     do
     {
-        // Menu de opções
+        // Menu de opções - Alteração no menu
         printf("\nMenu de Conversão:\n");
+        printf("Conversões a partir de Gramas:\n");
         printf("1. Gramas para Quilogramas\n");
-        printf("2. Gramas para Libras\n");
+        printf("2. Gramas para Toneladas\n");
+        printf("Conversões a partir de Quilogramas:\n");
         printf("3. Quilogramas para Gramas\n");
-        printf("4. Libras para Gramas\n");
-        printf("5. Quilogramas para Libras\n"); // adicionado nova conversão
-        printf("6. Libras para Quilogramas\n"); // adicionado nova conversão
+        printf("4. Quilogramas para Toneladas\n");
+        printf("Conversões a partir de Toneladas:\n");
+        printf("5. Toneladas para Gramas\n");
+        printf("6. Toneladas para Quilogramas\n");
         printf("0. Sair\n");
 
         // Leitura da opção escolhida pelo usuário
-        printf("Escolha opção desejada: ");
+        printf("Escolha a opção desejada: ");
         scanf("%d", &opcao);
 
         switch (opcao)
         {
-        case 1:
+        case 1: // Gramas para Quilogramas
             printf("Digite o valor em gramas: ");
             scanf("%lf", &valor);
             printf("%.2lf gramas equivalem a %.2lf quilogramas.\n", valor, gramas_para_quilogramas(valor));
-            break; // adicionado break, código estava dando erro
-        case 2:
-            // ... (outros casos)
-        case 3:
-            // ... (outros casos)
-        case 4:
-            printf("Digite o valor em libras: ");
+            break;
+        case 2: // Gramas para Toneladas
+            printf("Digite o valor em gramas: ");
             scanf("%lf", &valor);
-            printf("%.2lf libras equivalem a %.2lf gramas.\n", valor, libras_para_quilogramas(valor) * 1000.0);
-            break; // adicionado break, código estava dando erro
-        case 5:
-            // ... (outros casos)
-        case 6:
-            printf("Digite o valor em libras: ");
+            printf("%.2lf gramas equivalem a %.2lf toneladas.\n", valor, gramas_para_toneladas(valor));
+            break;
+        case 3: // Quilogramas para Gramas
+            printf("Digite o valor em quilogramas: ");
             scanf("%lf", &valor);
-            printf("%.2lf libras equivalem a %.2lf quilogramas.\n", valor, libras_para_quilogramas(valor));
-            break; // adicionado break, código estava dando erro
+            printf("%.2lf quilogramas equivalem a %.2lf gramas.\n", valor, quilogramas_para_gramas(valor));
+            break;
+        case 4: // Quilogramas para Toneladas
+            printf("Digite o valor em quilogramas: ");
+            scanf("%lf", &valor);
+            printf("%.2lf quilogramas equivalem a %.2lf toneladas.\n", valor, quilogramas_para_toneladas(valor));
+            break;
+        case 5: // Toneladas para Gramas
+            printf("Digite o valor em toneladas: ");
+            scanf("%lf", &valor);
+            printf("%.2lf toneladas equivalem a %.2lf gramas.\n", valor, toneladas_para_gramas(valor));
+            break;
+        case 6: // Toneladas para Quilogramas
+            printf("Digite o valor em toneladas: ");
+            scanf("%lf", &valor);
+            printf("%.2lf toneladas equivalem a %.2lf quilogramas.\n", valor, toneladas_para_quilogramas(valor));
+            break;
         case 0:
             printf("Saindo...\n");
             break;
